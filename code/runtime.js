9ba909d75a089f9bd37e93c6e1883010c4932122
adieuKai <hksc2j@163.com>
2023-02-19 20:53:52 +0800

    更新代码

--- code/runtime.js
+++ code/runtime.js
@@ -1,39 +1,13 @@
-setInterval(() => {
-    let create_time = Math.round(new Date('2023 2-18 00:00:00').getTime() / 1000); //在此行修改建站时间
-    let timestamp = Math.round((new Date().getTime()) / 1000);
-    let second = timestamp - create_time;
-    let time = new Array(0, 0, 0, 0, 0);
-    //格式规范化，个位数前面加0
-    var nol = function(h){
-      return h>9?h:'0'+h;
-    }
-    if (second >= 365 * 24 * 3600) {
-      time[0] = parseInt(second / (365 * 24 * 3600));
-      second %= 365 * 24 * 3600;
-    }//年
-    if (second >= 24 * 3600) {
-      time[1] = parseInt(second / (24 * 3600));
-      second %= 24 * 3600;
-    }//天
-    if (second >= 3600) {
-      time[2] = nol(parseInt(second / 3600));
-      second %= 3600;
-    }//时
-    if (second >= 60) {
-      time[3] = nol(parseInt(second / 60));
-      second %= 60;
-    }//分
-    if (second > 0) {
-      time[4] = nol(second);
-    }//秒
-    //早上7点到晚上10点营业
-    if ((Number(time[2])<22) && (Number(time[2])>7)){
-      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/河边别苑-上班工作中-6adea8?style=social&logo=cakephp' title='上班赚钱ing~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
-    } //徽标内容参考站内教程
-    //其余时间打烊
-    else{
-      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/河边别苑-下班休息了-6adea8?style=social&logo=coffeescript' title='下班了就该好好休息呀~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>'; //徽标内容参考站内教程
-    }
-    //覆写挂载标签的内容
-    document.getElementById("workboard").innerHTML = currentTimeHtml;
-  }, 1000);
+var now=new Date;
+function createtime()
+{now.setTime(now.getTime()+1e3);var e=new Date("08/01/2022 00:00:00"),
+t=Math.trunc(234e8+(now-e)/1e3*17),a=(t/1496e5).toFixed(6),
+o=new Date("08/09/2022 00:00:00"),n=(now-o)/1e3/60/60/24,
+r=Math.floor(n),i=(now-o)/1e3/60/60-24*r,s=Math.floor(i);
+1==String(s).length&&(s="0"+s);var d=(now-o)/1e3/60-1440*r-60*s,l=Math.floor(d);
+1==String(l).length&&(l="0"+l);var g=(now-o)/1e3-86400*r-3600*s-60*l,b=Math.round(g);
+1==String(b).length&&(b="0"+b);let c="";
+c=s<18&&s>=9?`<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/河边小院-上班摸鱼中.svg' title='努力赚钱ing~'>
+<br> <div style="font-size:13px;font-weight:bold">本站运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i></div>`:
+`<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/河边小院-下班休息啦.svg' title='下班了就该好好休息~'>
+<br> <div style="font-size:13px;font-weight:bold">本站运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> </div>`,document.getElementById("workboard")&&(document.getElementById("workboard").innerHTML=c)}setInterval((()=>{createtime()}),1e3);
