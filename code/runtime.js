dfc9de3546389d9814e6b74d007296607a8fe205
adieuKai <hksc2j@163.com>
2023-02-19 21:09:14 +0800

    更新代码

--- code/runtime.js
+++ code/runtime.js
@@ -16,7 +16,9 @@ function createtime()
   var g=(now-o)/1e3-86400*r-3600*s-60*l,
   b=Math.round(g);1==String(b).length&&(b="0"+b);
   let c=""
-c=s<17&&s>=9?`<img class='boardsign' src='https://img.shields.io/badge/河边小院-上班摸鱼中-6adea8?style=social&logo=cakephp' title='努力赚钱ing~'>
-<br> <div style="font-size:13px;font-weight:bold">本站已经运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i></div>`:
+c=s<17&&s>=9?
+`<img class='boardsign' src='https://img.shields.io/badge/河边小院-上班摸鱼中-6adea8?style=social&logo=cakephp' title='努力赚钱ing~'>
+<br> <div style="font-size:13px;font-weight:bold">本站已经运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 </div>`:
 `<img class='boardsign' src='https://img.shields.io/badge/河边小院-下班休息啦-6adea8?style=social&logo=cakephp' title='下班了就该好好休息~'>
-<br> <div style="font-size:13px;font-weight:bold">本站已经运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> </div>`,document.getElementById("workboard")&&(document.getElementById("workboard").innerHTML=c)}setInterval((()=>{createtime()}),1e3);
+<br> <div style="font-size:13px;font-weight:bold">本站已经运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 </div>`,
+document.getElementById("workboard")&&(document.getElementById("workboard").innerHTML=c)}setInterval((()=>{createtime()}),1e3);
